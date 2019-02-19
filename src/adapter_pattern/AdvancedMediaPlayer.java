b620e5912fae683d56739aac8c6a486e26bd3346
向绍谷 <1069342827@qq.com>
2019-02-19 17:54:09 +0800

    适配器模式

---
+++ src/adapter_pattern/AdvancedMediaPlayer.java
@@ -0,0 +1,9 @@
+package adapter_pattern;
+
+public interface AdvancedMediaPlayer {
+
+    public void playVlc(String fileName);
+
+    public void playMp4(String fileName);
+
+}
---
+++ src/adapter_pattern/MediaPlayer.java
@@ -0,0 +1,5 @@
+package adapter_pattern;
+
+public interface MediaPlayer {
+
+}
